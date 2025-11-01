cb2cf40730f925ea1dce2786608b25390668a7ff
Jack Zampolin <jack.zampolin@gmail.com>
2025-11-01 10:10:46 -0700

    fix: update tools/__init__ exports after processor refactor

--- pipeline/label_pages/tools/__init__.py
+++ pipeline/label_pages/tools/__init__.py
@@ -1,7 +1,8 @@
-from .processor import label_pages
+from .handlers import create_stage1_handler, create_stage2_handler
 from .report_generator import generate_report
 
 __all__ = [
-    "label_pages",
+    "create_stage1_handler",
+    "create_stage2_handler",
     "generate_report",
 ]
